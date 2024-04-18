4aded389992b36dd5a2268be37dc63137ae66f1b
marytishphillips <mphillip@trinity.edu>
2024-04-18 11:36:04 -0500

    changes to table

--- server/app/controllers/TableController.scala
+++ server/app/controllers/TableController.scala
@@ -3,9 +3,11 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 import play.api.libs.json._
+import slick.jdbc.PostgresProfile.api._
 import models.Tables._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import org.mindrot.jbcrypt.BCrypt
 
 @Singleton
 class WorkoutController @Inject()(cc: ControllerComponents, dbConfigProvider: play.api.db.slick.DatabaseConfigProvider) extends AbstractController(cc) {
@@ -18,16 +20,16 @@ class WorkoutController @Inject()(cc: ControllerComponents, dbConfigProvider: pl
   implicit val usersRowFormat = Json.format[UsersRow]
   implicit val workoutsRowFormat = Json.format[WorkoutsRow]
 
-  // API endpoint for user registration
-  def registerUser = Action.async(parse.json) { request =>
-    request.body.validate[UsersRow].fold(
-      errors => Future.successful(BadRequest("Invalid User Data")),
-      user => db.run(Users += user).map(_ => Ok("User registered successfully"))
-    )
+   // API endpoint for user login
+  def validateUser(username: String, password: String): Future[Option[Int]] = {
+    val matches = db.run(Users.filter(userRow => userRow.username === username).result)
+    matches.map(userRows => userRows.headOption.flatMap {
+      userRow => if (BCrypt.checkpw(password, userRow.password)) Some(userRow.id) else None
+    })
   }
 
-
-  /*def createUser(username: String, password: String): Future[Option[Int]] = {
+  // API endpoint for user registration
+  def createUser(username: String, password: String): Future[Option[Int]] = {
     val matches = db.run(Users.filter(userRow => userRow.username === username).result)
     matches.flatMap { userRows =>
       if (userRows.isEmpty) {
@@ -39,16 +41,16 @@ class WorkoutController @Inject()(cc: ControllerComponents, dbConfigProvider: pl
           }
       } else Future.successful(None)
     }
-  }*/
+  }
 
-  // API endpoint for user login
-  def loginUser(username: String, password: String) = Action.async {
+ 
+ /* def loginUser(username: String, password: String) = Action.async {
     val query = Users.filter(u => u.username === username && u.password === password).result.headOption
     db.run(query).map {
       case Some(user) => Ok(Json.toJson(user))
       case None => Unauthorized("Invalid credentials")
     }
-  }
+  }*/
 
   // API endpoint to get workouts based on user preferences
   def getWorkouts(sweatLevel: Int, intensity: Int, workLength: Int) = Action.async {
