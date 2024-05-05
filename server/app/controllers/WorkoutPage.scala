16671d3388e79c81a5fc33a792069371e40c924e
vividawg <viviram035@gmail.com>
2024-05-05 13:17:26 -0500

    testing push from ssh repo

--- server/app/controllers/WorkoutPage.scala
+++ server/app/controllers/WorkoutPage.scala
@@ -6,29 +6,30 @@ import shared.SharedMessages
 import play.api.mvc._
 import play.api.i18n._
 
+//added comment to test push to test if we can push from vscode with ssh repo
 @Singleton
-class WorkoutPage @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-    def home = Action {
-        Ok(views.html.home())
-    }
-    def login = Action {
-        Ok(views.html.login())
-    }
-    def signUp = Action {
-        Ok(views.html.signUp())
-    }
-    def profile = Action {
-        Ok(views.html.profile())
-    }
-    def search = Action {
-        Ok(views.html.search())
-    }
-    def myVideos = Action {
-        Ok(views.html.myVideos())
-    }
-    def video = Action {
-        Ok(views.html.video())
-    }
-
+class WorkoutPage @Inject() (cc: ControllerComponents)
+    extends AbstractController(cc) {
+  def home = Action {
+    Ok(views.html.home())
+  }
+  def login = Action {
+    Ok(views.html.login())
+  }
+  def signUp = Action {
+    Ok(views.html.signUp())
+  }
+  def profile = Action {
+    Ok(views.html.profile())
+  }
+  def search = Action {
+    Ok(views.html.search())
+  }
+  def myVideos = Action {
+    Ok(views.html.myVideos())
+  }
+  def video = Action {
+    Ok(views.html.video())
+  }
 
 }
